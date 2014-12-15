0d2a890a3fdcee7b5a38812bcc4a606c8e8dd24a
Nick Mealey <mealey@partstash.com>
2014-12-15 10:54:53 -0800

    Added route for about.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome, pip' });
+});
+
+// About
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'About Nodle'})
 });
 
 module.exports = router;
